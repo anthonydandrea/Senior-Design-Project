e9e75cb33cd16bca52fdaa09f048bf537dec86d4
mlu4679 <44623179+mlu4679@users.noreply.github.com>
2020-03-19 20:26:58 -0400

    hard rules'

--- Rules_data/hard_rules1.py
+++ Rules_data/hard_rules1.py
@@ -1,12 +1,12 @@
 import csv
-
+import usaddress
 with open('rules_states.csv', mode = 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
     states_dict = {}
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
-            states_dict[row[0].lower()] = row[1].lower()
+            states_dict[row[0].lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})] = row[1].lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
         line_count +=1 
 with open('rules_baby-names.csv', mode = 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
@@ -14,7 +14,7 @@ with open('rules_baby-names.csv', mode = 'r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count != 0 and int(row[0]) > 1940:
-            names_set.add(row[1].lower())
+            names_set.add(row[1].lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "}))
         line_count +=1
 
 f = open('rules_countries.txt','r')
@@ -22,7 +22,7 @@ country_set = set()
 line_count = 0
 f1 = f.readlines() 
 for row in f1:    
-    country_set.add(row.lower()[:-1])
+    country_set.add(row.lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})[:-1])
     line_count +=1
 
 with open('surnames.csv', mode = 'r') as csv_file:
@@ -31,7 +31,7 @@ with open('surnames.csv', mode = 'r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
-            lnames_set.add(row[0].lower())
+            lnames_set.add(row[0].lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "}))
         line_count +=1
 
 with open('uscities.csv', mode = 'r') as csv_file:
@@ -40,7 +40,7 @@ with open('uscities.csv', mode = 'r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
-            city_set.add(row[0].lower())
+            city_set.add(row[0].lower().strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "}))
         line_count +=1
 
 with open('suffixes.csv', mode = 'r') as csv_file:
@@ -49,7 +49,7 @@ with open('suffixes.csv', mode = 'r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
-            suffix_set[row[0]] = row[1]
+            suffix_set[row[0].strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})] = row[1].strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
         line_count +=1
 
 with open('prefixes.csv', mode = 'r') as csv_file:
@@ -58,20 +58,20 @@ with open('prefixes.csv', mode = 'r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
-            prefix_set[row[0]] = row[1]
+            prefix_set[row[0].strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})] = row[1].strip().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
         line_count +=1
 
 
 def check_states(state):
-    return state in states_dict
+    return state.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "}) in states_dict
 
 def check_country(country):
-    return country  in country_set
+    return country.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in country_set
 
 def check_fname(names):
-    return names.lower() in names_set
+    return names.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in names_set
 def check_lname(names):
-    return names.lower() in lnames_set
+    return names.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in lnames_set
 def check_ssn(ssn):
     if len(ssn) == 11 or  len(ssn) == 9:
         if len(ssn) == 11:
@@ -89,17 +89,31 @@ def check_zip(zip):
     return False
 
 def check_suffix(suff):
-    return (suff.lower() in suffix_set)
+    return (suff.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in suffix_set)
 def check_prefix(pre):
-    return (pre.lower() in prefix_set)
+    return (pre.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in prefix_set)
 def check_city(city):
-    return (city.lower() in city_set)
+    return (city.lower().translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})  in city_set)
 
 def check_street(street):
-    x = street.split()
-    flag = False
-    for z in range(1, len(x)):
-        if not x[z].isdigit() and isinstance(x[z], str):
-            flag = True
-    return (x[0].isdigit()  and flag)    
-    
+    x = usaddress.parse(street)
+    num = False
+    streetname = False
+    post = False
+    for item in x:
+        if item[1] == 'StreetNamePostType':
+            post = True
+        if item[1] == "StreetName":
+            streetname = True
+        if item[1] == "AddressNumber":
+            num = True
+    return num and streetname and post
+    # x = street.split()
+    # flag = False
+    # for z in range(1, len(x)):
+    #     if not x[z].isdigit() and isinstance(x[z], str):
+    #         flag = True
+    # return (x[0].isdigit()  and flag)    
+
+
+print(check_street("1234 Fake address"), check_states("arIZON    a@@@"))
