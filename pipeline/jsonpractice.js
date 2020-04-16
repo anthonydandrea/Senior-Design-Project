a5904827aae8644b66133e4b1a8eb57d7b56cac1
riedling <starlightandrobots19@gmail.com>
2020-04-16 18:44:26 -0400

    json write to js file for visual output

---
+++ pipeline/jsonpractice.js
@@ -0,0 +1,14 @@
+var nodes = [
+	{id: 0, label: f_name, group: 0},
+	{id: 1, label: fname, group: 0},
+	{id: 2, label: addr, group: 1},
+	{id: 3, label: first_name, group: 0},
+	];
+
+var edges = [
+	{from: 0, to: 0, label: 53.0%},
+	{from: 1, to: 0, label: 65.0%},
+	{from: 2, to: 1, label: 19.0%},
+	{from: 3, to: 0, label: 78.0%},
+	];
+
