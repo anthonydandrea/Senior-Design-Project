99f5cd2d489f043ba31e2da857bc85df9b3b3623
riedling <starlightandrobots19@gmail.com>
2020-04-17 23:20:28 -0400

    fixed json and js error, moved javascript writer to different file

---
+++ pipeline/js_utilities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import json_utilities as json
+
+def js_write(write_file_path, read_file_path):
+	data = json.json_read(read_file_path)
+	open(write_file_path, 'w').close()
+	with open(write_file_path, 'a') as outfile:
+		_format_javascript(outfile, data)
+
+def _format_javascript(f, data):
+	formatted_var = ["var nodes = [\n\t"]
+	formatted_edges = ["var edges = [\n\t"]
+	for entry in data:
+		nextnode = "{id: %d, label: %s, group: %s},\n\t" %(entry["id"], entry["field_name"], entry["cluster"])
+		formatted_var.append(nextnode)
+		nextedge = "{from: %d, to: %d, label: %s},\n\t" %(entry["id"], entry["cluster"], str(entry["likelihood"]*100)+"%")
+		formatted_edges.append(nextedge)
+	formatted_var.append("];\n\n")
+	formatted_edges.append("];\n\n")
+	for x in formatted_var:
+		f.write(x)
+	for y in formatted_edges:
+		f.write(y)
+	return
+
+js_write("./jsonpractice.js", "./sample.json")
--- pipeline/json_utilities.py
+++ pipeline/json_utilities.py
@@ -3,30 +3,9 @@ import json
 def json_read(json_file_path):
 	data = None
 	with open(json_file_path) as json_file:
-    		data = json.load(json_file)
+		data = json.load(json_file)
 	return data
 
-def json_write(write_file_path, read_file_path):
-	data = json_read(read_file_path)
-	open(write_file_path, 'w').close()
-	with open(write_file_path, 'a') as outfile:
-		_format_json(outfile, data)
-		#json.dump(data, outfile)
-
-def _format_json(f, data):
-	formatted_var = ["var nodes = [\n\t"]
-	formatted_edges = ["var edges = [\n\t"]
-	for entry in data:
-		nextnode = "{id: %d, label: %s, group: %s},\n\t" %(entry["id"], entry["field_name"], entry["cluster"])
-		formatted_var.append(nextnode)
-		nextedge = "{from: %d, to: %d, label: %s},\n\t" %(entry["id"], entry["cluster"], str(entry["likelihood"]*100)+"%")
-		formatted_edges.append(nextedge)
-	formatted_var.append("];\n\n")
-	formatted_edges.append("];\n\n")
-	for x in formatted_var:
-		f.write(x)
-	for y in formatted_edges:
-		f.write(y)
-	return
-
-#json_write("./jsonpractice.js", "./sample.json")
+def json_write(json_file_path, data):
+	with open(json_file_path, 'w') as outfile:
+		json.dump(data, outfile)
